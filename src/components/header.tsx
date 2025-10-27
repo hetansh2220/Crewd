a8fdfb6f25e0584db5119a4924d751fb7ffc42ef
Meet <mrmit4040@gmail.com>
2025-10-27 18:39:47 +0530

    feat-changed logo and its text color while switching themes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -102,7 +102,7 @@ export function Header() {
               width={128}
               height={128}
               alt="Crewd Logo"
-              className="h-32 w-32 cursor-pointer"
+              className={`h-32 w-32 cursor-pointer transition-all ${theme === 'light' ? 'invert' : ''}`}
               onClick={() => router.push("/")}
             />
           </div>
